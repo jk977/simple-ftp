74eb78a285993726a2d2d4bb978b66f8df8d545f
jk977 <jdk64@comcast.net>
2020-04-22 10:35:51 -0700

    Adding server setup (bind, listen)

--- src/mftpserve.c
+++ src/mftpserve.c
@@ -1,3 +1,4 @@
+#include "config.h"
 #include "util.h"
 
 #include <stdlib.h>
@@ -14,7 +15,21 @@ int run_server(void)
     int const sock = make_socket(&info);
     FAIL_IF(sock < 0, "make_socket", EXIT_FAILURE);
 
-    // TODO: bind server, implement server control commands, etc.
+    struct in_addr const sin_addr = { .s_addr = INADDR_ANY };
+
+    struct sockaddr_in address = {
+        .sin_family = AF_INET,
+        .sin_port = htons(CFG_PORT),
+        .sin_addr = sin_addr,
+    };
+
+    // set up server to start listening for connections (any address)
+    FAIL_IF(bind(sock, (struct sockaddr*) &address, sizeof(address)) < 0,
+            "bind", EXIT_FAILURE);
+    FAIL_IF(listen(sock, CFG_BACKLOG) < 0,
+            "listen", EXIT_FAILURE);
+
+    // TODO: implement server control commands, etc.
 
     return EXIT_FAILURE;
 }
